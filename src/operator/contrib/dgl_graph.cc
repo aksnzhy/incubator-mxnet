4882fe9bc8f77d625e1e1515cd97f7590d70f17b
aksnzhy <mctt90@gmail.com>
2018-12-07 16:09:48 +0800

    update

--- src/operator/contrib/dgl_graph.cc
+++ src/operator/contrib/dgl_graph.cc
@@ -583,7 +583,6 @@ static void SampleSubgraph(const NDArray &csr,
     node.vertex_id = seed[i];
     node.level = 0;
     node_queue.push(node);
-    sub_ver_mp[node.vertex_id] = node.level;
   }
   std::vector<dgl_id_t> tmp_sampled_src_list;
   std::vector<dgl_id_t> tmp_sampled_edge_list;
@@ -597,11 +596,10 @@ static void SampleSubgraph(const NDArray &csr,
     ver_node& cur_node = node_queue.front();
     dgl_id_t dst_id = cur_node.vertex_id;
     auto ret = sub_ver_mp.find(dst_id);
-    /*
     if (ret != sub_ver_mp.end()) {
       node_queue.pop();
       continue;
-    }*/
+    }
     tmp_sampled_src_list.clear();
     tmp_sampled_edge_list.clear();
     dgl_id_t ver_len = *(indptr+dst_id+1) - *(indptr+dst_id);
@@ -638,7 +636,7 @@ static void SampleSubgraph(const NDArray &csr,
       neighbor_list.push_back(tmp_sampled_edge_list[i]);
     }
     num_edges += tmp_sampled_src_list.size();
-    //sub_ver_mp[cur_node.vertex_id] = cur_node.level;
+    sub_ver_mp[cur_node.vertex_id] = cur_node.level;
     for (size_t i = 0; i < tmp_sampled_src_list.size(); ++i) {
       auto ret = sub_ver_mp.find(tmp_sampled_src_list[i]);
       if (ret == sub_ver_mp.end()) {
@@ -651,8 +649,8 @@ static void SampleSubgraph(const NDArray &csr,
           size_t pos = neighbor_list.size();
           neigh_pos[new_node.vertex_id] = pos;
           neighbor_list.push_back(0);
+          sub_ver_mp[new_node.vertex_id] = new_node.level;
         }
-        sub_ver_mp[new_node.vertex_id] = new_node.level;
       }
     }
     sub_vertices_count++;
